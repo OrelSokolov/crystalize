64584eccc771f34c13d658a7fd5d94539a73217f
Oleg Orlov <orelcokolov@gmail.com>
2019-08-31 18:17:33 +0300

    Update specs and etc

---
+++ lib/crystalize/converter.rb
@@ -0,0 +1,5 @@
+module Crystalize
+  class Converter
+    include Crystalize::Literals
+  end
+end
---
+++ lib/crystalize/literals.rb
@@ -0,0 +1,11 @@
+module Crystalize
+  module Literals
+    def array
+
+    end
+
+    def hashes
+
+    end
+  end
+end
