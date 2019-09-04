861a820bbe16ea5e36923397b11a54fe1083f638
Oleg Orlov <orelcokolov@gmail.com>
2019-09-04 22:51:47 +0300

    Update runner

--- lib/crystalize/project_converter.rb
+++ lib/crystalize/project_converter.rb
@@ -6,10 +6,10 @@ module Crystalize
 
     def convert
       files = @options[:files] ? Dir[@options[:files]] : []
-      files.select{|f| f.ends_with?(".rb") }.each do |file|
+      files.select{|f| f.end_with?(".rb") }.each do |file|
         converter =  CodeConverter.new(@options, File.read(file))
         converter.convert
-        result = converter.new_content
+        result = converter.new_content_string
         File.open(file, 'w') {|f| f.write result}
       end
     end
